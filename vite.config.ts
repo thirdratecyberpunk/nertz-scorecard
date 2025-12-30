ab9ffb2869054b5c0a79a0d6751154c2471ac80d
thirdratecyberpunk <blackbul@aston.ac.uk>
2025-12-30 00:43:12 +0000

    Fixed missing config in the vite

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
+  base: '/nertz-scorecard/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
